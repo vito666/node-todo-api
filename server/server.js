2ae9b759b7f90d3b0d40d31c4e7237c1ed9e58d8
Artem Konstantinov <don666vito@yandex.ua>
2017-09-25 16:40:39 +0300

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,9 +49,9 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   // Will be deleted at the end ( For debugging purposes only )
-  console.log('Started on port 3000');
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = { app };
